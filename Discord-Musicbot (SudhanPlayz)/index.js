c26e7ab119ba5f88b98c026980c9e9c5f19d1f69
Daniyt_th <viragdaniel@outlook.com>
2026-01-23 13:44:52 +0100

    Zene bot fordítás kezdése

--- Discord-Musicbot (SudhanPlayz)/index.js
+++ Discord-Musicbot (SudhanPlayz)/index.js
@@ -5,11 +5,11 @@ const DiscordMusicBot = require("./lib/DiscordMusicBot");
 const { exec } = require("child_process");
 
 if (process.env.REPL_ID) {
-	console.log("Replit system detected, initiating special `unhandledRejection` event listener.")
+	console.log("Replit rendszer észlelve, speciális `unhandledRejection` eseményfigyelő indítása.")
 	process.on('unhandledRejection', (reason, promise) => {
 		promise.catch((err) => {
 			if (err.status === 429) {
-				console.log("something went wrong whilst trying to connect to discord gateway, resetting...");
+				console.log("Valami probléma történt a Discord Gateway-hez való csatlakozás során, újraindítás...");
 				exec("kill 1");
 			}
 		});
@@ -18,7 +18,7 @@ if (process.env.REPL_ID) {
 
 const client = new DiscordMusicBot();
 
-console.log("Make sure to fill in the config.js before starting the bot.");
+console.log("A bot elindítása előtt feltétlenül töltse ki a config.js fájlt.");
 
 const getClient = () => client;
 
